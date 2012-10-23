ca36762f1121df11354f15f3928f1207f2088bb2
George Notaras <gnot@g-loaded.eu>
2012-10-23 11:41:10 +0300

    Added ModelAdmin interfaces.

--- src/powerdns_manager/admin.py
+++ src/powerdns_manager/admin.py
@@ -26,4 +26,127 @@
 
 from django.contrib import admin
 from django.db.models.loading import cache
+from django.contrib import messages
+from django.contrib.admin import SimpleListFilter
+
+# Action for
+# - set change date
+# - set serial (?)
+# - set TTL to 300, 3600, 86400
+#
+#def test_action(modeladmin, request, queryset):
+#    messages.add_message(request, messages.INFO, 'The test action was successful.')
+#test_action.short_description = "Test Action"
+
+
+class OwnDomainListFilter(SimpleListFilter):
+    title = _('domain')
+    parameter_name = 'domain'
+
+    def lookups(self, request, model_admin):
+        Domain = cache.get_model('powerdns_manager', 'Domain')
+        qs = Domain.objects.filter(created_by=request.user)
+        for namespace in qs:
+            yield (namespace, namespace)
+
+    def queryset(self, request, queryset):
+        the_domain = self.value()
+        if the_domain:
+            return queryset.filter(domain__name=the_domain, domain__created_by=request.user)
+        return queryset
+
+
+class RecordInline(admin.TabularInline):
+    model = cache.get_model('powerdns_manager', 'Record')
+    fields = ('name', 'type', 'ttl', 'prio', 'content', 'auth', 'date_modified')
+    readonly_fields = ('date_modified', )
+    extra = 1
+
+class DomainMetadataInline(admin.TabularInline):
+    model = cache.get_model('powerdns_manager', 'DomainMetadata')
+    fields = ('kind', 'content', 'date_modified')
+    readonly_fields = ('date_modified', )
+    extra = 1
+    
+
+class DomainAdmin(admin.ModelAdmin):
+    #form = DomainModelForm
+    #actions = [reload_php_stack, ]
+    
+    fieldsets = (
+        ('', {
+            'fields' : ('name', 'type', 'master'),
+            #'description' : 'Main virtual host attributes',
+        }),
+        ('Info', {
+            'classes' : ('collapse',),
+            'fields' : ('date_created', 'date_modified', ),
+            #'description' : 'Information about the domain.',
+        }),
+    )
+    readonly_fields = ('date_created', 'date_modified', )
+    list_display = ('name', 'type', 'master', 'date_created', 'date_modified')
+    list_filter = ('type', )
+    search_fields = ('name', 'master')
+    inlines = [RecordInline, DomainMetadataInline]
+    
+    def queryset(self, request):
+        qs = super(DomainAdmin, self).queryset(request)
+        if not request.user.is_superuser:
+            # Non-superusers see the domains they have created
+            qs = qs.filter(name__created_by=request.user)
+        return qs
+    
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+        obj.save()
+
+admin.site.register(cache.get_model('powerdns_manager', 'Domain'), DomainAdmin)
+
+
+
+
+
+    
+
+
+class TsigKeyAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('', {
+            'fields' : ('name', 'algorithm', 'secret', ),
+        }),
+        ('Info', {
+            'classes' : ('collapse',),
+            'fields' : ('date_created', 'date_modified', ),
+        }),
+    )
+    readonly_fields = ('date_created', 'date_modified')
+    list_display = ('name', 'algorithm', 'date_created', 'date_modified')
+    list_filter = ('algorithm', )
+    search_fields = ('name', )
+    
+    def queryset(self, request):
+        qs = super(TsigKeyAdmin, self).queryset(request)
+        if not request.user.is_superuser:
+            # Non-superusers see the records they have created
+            qs = qs.filter(created_by=request.user)
+        return qs
+    
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+        obj.save()
+    
+admin.site.register(cache.get_model('powerdns_manager', 'TsigKey'), TsigKeyAdmin)
+
+
+
+class SuperMasterAdmin(admin.ModelAdmin):
+    fields = ('ip', 'nameserver', 'account')
+    readonly_fields = ('date_created', 'date_modified')
+    list_display = ('ip', 'nameserver', 'account', 'date_created', 'date_modified')
+    search_fields = ('nameserver', 'account')
+    
+admin.site.register(cache.get_model('powerdns_manager', 'SuperMaster'), SuperMasterAdmin)
 
