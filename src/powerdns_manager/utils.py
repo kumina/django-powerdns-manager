00de8c80c6331336dcd3e310187c87604366f858
George Notaras <gnot@g-loaded.eu>
2012-10-29 18:37:20 +0200

    Added b32hex_encode() utility function.

--- src/powerdns_manager/utils.py
+++ src/powerdns_manager/utils.py
@@ -428,7 +428,80 @@ def rectify_zone(origin):
         rr.save()
 
 
+
+
+
+# Base32 encoding/decoding must be done in Python
+_b32alphabet = {
+    0: '0',  9: '9', 18: 'I', 27: 'R',
+    1: '1', 10: 'A', 19: 'J', 28: 'S',
+    2: '2', 11: 'B', 20: 'K', 29: 'T',
+    3: '3', 12: 'C', 21: 'L', 30: 'U',
+    4: '4', 13: 'D', 22: 'M', 31: 'V',
+    5: '5', 14: 'E', 23: 'N',
+    6: '6', 15: 'F', 24: 'O',
+    7: '7', 16: 'G', 25: 'P',
+    8: '8', 17: 'H', 26: 'Q',
+    }
+
+_b32tab = _b32alphabet.items()
+_b32tab.sort()
+_b32tab = [v for k, v in _b32tab]
+_b32rev = dict([(v, long(k)) for k, v in _b32alphabet.items()])
+
+
+def b32hex_encode(s):
+    """Encode a string using Base32hex.
+
+    s is the string to encode.  The encoded string is returned.
     
+    ``base32hex`` encoding is described in Section 7 of RFC4648
     
+        http://tools.ietf.org/html/rfc4648#section-7
     
+    This function is licensed under the terms of the:
     
+        PYTHON SOFTWARE FOUNDATION LICENSE VERSION 2
+    
+    It has derived from the b32encode() function of the ``base64`` module
+    of the Standard Library.
+    
+    ``_b32alphabet`` has been updated to the alphabet that is included in
+    Section 7 of RFC4648.
+    
+    """
+    parts = []
+    quanta, leftover = divmod(len(s), 5)
+    # Pad the last quantum with zero bits if necessary
+    if leftover:
+        s += ('\0' * (5 - leftover))
+        quanta += 1
+    for i in range(quanta):
+        # c1 and c2 are 16 bits wide, c3 is 8 bits wide.  The intent of this
+        # code is to process the 40 bits in units of 5 bits.  So we take the 1
+        # leftover bit of c1 and tack it onto c2.  Then we take the 2 leftover
+        # bits of c2 and tack them onto c3.  The shifts and masks are intended
+        # to give us values of exactly 5 bits in width.
+        c1, c2, c3 = struct.unpack('!HHB', s[i*5:(i+1)*5])
+        c2 += (c1 & 1) << 16 # 17 bits wide
+        c3 += (c2 & 3) << 8  # 10 bits wide
+        parts.extend([_b32tab[c1 >> 11],         # bits 1 - 5
+                      _b32tab[(c1 >> 6) & 0x1f], # bits 6 - 10
+                      _b32tab[(c1 >> 1) & 0x1f], # bits 11 - 15
+                      _b32tab[c2 >> 12],         # bits 16 - 20 (1 - 5)
+                      _b32tab[(c2 >> 7) & 0x1f], # bits 21 - 25 (6 - 10)
+                      _b32tab[(c2 >> 2) & 0x1f], # bits 26 - 30 (11 - 15)
+                      _b32tab[c3 >> 5],          # bits 31 - 35 (1 - 5)
+                      _b32tab[c3 & 0x1f],        # bits 36 - 40 (1 - 5)
+                      ])
+    encoded = EMPTYSTRING.join(parts)
+    # Adjust for any leftover partial quanta
+    if leftover == 1:
+        return encoded[:-6] + '======'
+    elif leftover == 2:
+        return encoded[:-4] + '===='
+    elif leftover == 3:
+        return encoded[:-3] + '==='
+    elif leftover == 4:
+        return encoded[:-1] + '='
+    return encoded
