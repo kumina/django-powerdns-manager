bb59c47f117f2413e63566690ca3e7fa38ddbebc
George Notaras <gnot@g-loaded.eu>
2012-11-03 14:49:25 +0200

    Set version to 0.1.17a1

--- src/powerdns_manager/__init__.py
+++ src/powerdns_manager/__init__.py
@@ -27,7 +27,7 @@
 # Scheme: <major>.<minor>.<maintenance>.<maturity>.<revision>
 # maturity: final/beta/alpha
 
-VERSION = (0, 1, 16, 'alpha', 1)
+VERSION = (0, 1, 17, 'alpha', 1)
 
 def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
